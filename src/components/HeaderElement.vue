114151d80b35a1df8adee9de5363cbb42a99a8d3
EMEA\LAGOPV <vlagop98@gmail.com>
2024-10-12 12:59:07 +0300

    Remove: Burger svg from Header Element

--- src/components/HeaderElement.vue
+++ src/components/HeaderElement.vue
@@ -6,11 +6,6 @@
           <img class="govgr-header__logo logo-image" src="https://www.gov.gr/gov_gr_logo.svg" alt="gov.gr logo" />
         </a>
       </div>
-      <svg class="govgr-svg-icon govgr-svg-icon--burger" focusable="true" viewBox="0 0 24 24" aria-hidden="true">
-        <rect id="govgr-svg-icon--burger__line-1" width="20" height="3.3" />
-        <rect id="govgr-svg-icon--burger__line-2" width="20" height="3.3" />
-        <rect id="govgr-svg-icon--burger__line-3" width="20" height="3.3" />
-      </svg>
     </div>
   </header>
 </template>
