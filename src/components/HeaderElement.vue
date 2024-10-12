4de3069ead7487a86d725dc4196aaec9356fca4a
EMEA\LAGOPV <vlagop98@gmail.com>
2024-10-12 13:19:15 +0300

    Added: Service Name on the Header

--- src/components/HeaderElement.vue
+++ src/components/HeaderElement.vue
@@ -6,6 +6,7 @@
           <img class="govgr-header__logo logo-image" src="https://www.gov.gr/gov_gr_logo.svg" alt="gov.gr logo" />
         </a>
       </div>
+      <a href="/" class="govgr-header__title service-title">Υπολογισμός Παροχών</a>
     </div>
   </header>
 </template>
@@ -19,5 +20,8 @@ export default {
 <style scoped>
 .logo-image{
   width:50%
+}
+.service-title{
+  color:white
 }
 </style>
