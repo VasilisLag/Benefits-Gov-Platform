892cc01a5e8895d9026f47876ad8817cb74eca37
EMEA\LAGOPV <vlagop98@gmail.com>
2024-10-13 20:02:36 +0300

    Modify: Make Service name size bigger

--- src/components/Elements/HeaderElement.vue
+++ src/components/Elements/HeaderElement.vue
@@ -6,7 +6,7 @@
           <img class="govgr-header__logo logo-image" src="https://www.gov.gr/gov_gr_logo.svg" alt="gov.gr logo" />
         </a>
       </div>
-      <a href="/" class="govgr-header__title service-title">Υπολογισμός Παροχών</a>
+      <a href="/" class="govgr-header__title service-title govgr-!-font-size-24">Υπολογισμός Παροχών</a>
     </div>
   </header>
 </template>
