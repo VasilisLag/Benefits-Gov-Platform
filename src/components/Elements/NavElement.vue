b5e2f3782da6f90015d3b2332b874f04dd1f5b6a
EMEA\LAGOPV <vlagop98@gmail.com>
2024-10-14 11:07:14 +0300

    Modify: Rename one of the benefits

--- src/components/Elements/NavElement.vue
+++ src/components/Elements/NavElement.vue
@@ -4,7 +4,7 @@
       <li><a href="" class="govgr-nav__list-item-link govgr-nav__list-item-link--active">Σχετικά</a></li>
       <li><a href="#" class="govgr-nav__list-item-link">Αξιολόγηση</a></li>
       <li><a href="#" class="govgr-nav__list-item-link">Επίδομα Παιδιού-Α21</a></li>
-      <li><a href="#" class="govgr-nav__list-item-link">Επίδομα Ενοικίου</a></li>
+      <li><a href="#" class="govgr-nav__list-item-link">Επίδομα Στέγασης</a></li>
       <li><a href="#" class="govgr-nav__list-item-link">Επίδομα Θέρμανσης</a></li>
       <li><a href="#" class="govgr-nav__list-item-link">ΚΕΠΑ</a></li>
     </ul>
