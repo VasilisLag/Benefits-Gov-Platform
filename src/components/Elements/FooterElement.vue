7ae996351e036c50cf95668c93aa75eb8ac15fc6
EMEA\LAGOPV <vlagop98@gmail.com>
2024-10-21 20:37:31 +0300

    Add: Footer Element created

---
+++ src/components/Elements/FooterElement.vue
@@ -0,0 +1,30 @@
+<template>
+    <footer class="govgr-footer" role="contentinfo">
+        <div class="govgr-width-container">
+            <div class="govgr-footer__meta">
+                <div class="govgr-footer__meta-item govgr-footer__meta-item--grow">
+                    <div class="govgr-footer__content">
+                        <p class="govgr-footer__licence-description">
+                            Υλοποίηση από το 
+                            <a href="https://grnet.gr" target="_blank" rel="noreferrer noopener" class="govgr-link">ΕΔΥΤΕ<span class="govgr-visually-hidden">(ανοίγει σε καινούρια καρτέλα)</span></a> για το
+                            <a href="https://mindigital.gr/" target="_blank" rel="noreferrer noopener" class="govgr-link"> Υπουργείο Ψηφιακής Διακυβέρνησης <span class="govgr-visually-hidden">(ανοίγει σε καινούρια καρτέλα)</span></a>
+                        </p>
+                    </div>
+                </div>
+                <div class="govgr-footer__meta-item">
+                    <img class="govgr-footer__government-logo" src="@/assets/government-logo2.svg" alt="government logo"/>
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+  name: 'FooterElement'
+};
+</script>
+
+<style scoped>
+/* Add your styles here */
+</style>
