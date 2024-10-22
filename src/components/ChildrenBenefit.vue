4400bb82ed29ab56fc076e14c5785a462a981b6e
EMEA\LAGOPV <vlagop98@gmail.com>
2024-10-22 16:17:21 +0300

    Add: New page for Children Benefit

---
+++ src/components/ChildrenBenefit.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <HeaderElement />
+    <NavElement />
+    <main class="p-4">
+    </main>
+    <FooterElement/>
+  </div>
+</template>
+
+<script>
+import HeaderElement from '@/components/Elements/HeaderElement.vue';
+import NavElement from '@/components/Elements/NavElement.vue';
+import FooterElement from '@/components/Elements/FooterElement.vue';
+
+export default {
+  name: 'ChildrenBenefit',
+  components: {
+    HeaderElement,
+    NavElement,
+    FooterElement,
+  }
+};
+</script>
+
+<style scoped>
+section {
+  margin: 4vw;
+  text-align: left;
+}
+</style>
