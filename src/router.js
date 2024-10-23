b642e36af3d53ca3278feb258d3d427420e0e311
EMEA\LAGOPV <vlagop98@gmail.com>
2024-10-23 21:30:46 +0300

    Modify: More routes - one for each tab

--- src/router.js
+++ src/router.js
@@ -4,7 +4,11 @@ import ChildrenBenefit from './components/ChildrenBenefit.vue';
 
 const routes = [
   { path: '/', component: HomePage },
+  { path: '/SummaryBenefit', component: ChildrenBenefit },
   { path: '/ChildrenBenefit', component: ChildrenBenefit },
+  { path: '/HeatingBenefit', component: ChildrenBenefit },
+  { path: '/DisabilityBenefit', component: ChildrenBenefit },
+  { path: '/HousingBenefit', component: ChildrenBenefit },
 ];
 
 const router = createRouter({
