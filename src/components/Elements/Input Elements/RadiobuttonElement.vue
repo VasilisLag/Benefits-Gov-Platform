694e0212b17b8b857b65441826f3bf8fcd4ae5d0
EMEA\LAGOPV <vlagop98@gmail.com>
2024-11-07 18:36:18 +0200

    Fix: Navigation between Radiobuttons Elements is functional too

--- src/components/Elements/Input Elements/RadiobuttonElement.vue
+++ src/components/Elements/Input Elements/RadiobuttonElement.vue
@@ -2,8 +2,14 @@
   <div>
     <label>{{ question }}</label>
     <div v-for="option in options" :key="option">
-      <input type="radio" v-model="selectedOption" :value="option" @change="onChange(option)" />
-      {{ option }}
+      <input 
+        type="radio" 
+        v-model="selectedOption" 
+        :value="option" 
+        @change="onChange(selectedOption)" 
+        :id="option"
+      />
+      <label :for="option">{{ option }}</label>
     </div>
   </div>
 </template>
@@ -14,16 +20,31 @@ export default {
   props: {
     question: String,
     options: Array,
+    answer: {
+      type: String,
+      default: null
+    }
   },
   data() {
     return {
-      selectedOption: null,
+      selectedOption: this.answer || null // Initialize selectedOption with answer or null
     };
+  },
+  watch: {
+    // Watch for changes in the 'answer' prop and update selectedOption accordingly
+    answer(newAnswer) {
+      this.selectedOption = newAnswer;
+    }
   },
   methods: {
     onChange(value) {
+      // Emit the selected value whenever a radio button is selected
       this.$emit('onAnswerChange', value);
-    },
+    }
   },
+  mounted() {
+    // Emit the initial selected option when the component is mounted
+    this.$emit('onAnswerChange', this.selectedOption);
+  }
 };
 </script>
