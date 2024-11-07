46a746ff224f4f7b19804015d32df588f5acff30
EMEA\LAGOPV <vlagop98@gmail.com>
2024-11-07 14:06:20 +0200

    Modify: More questions for testing

--- src/questions/childrenBenefitQs.js
+++ src/questions/childrenBenefitQs.js
@@ -10,9 +10,39 @@ const questions = [
       category: "multiple-choice",
     },
     {
+      question: "Is your household income below $50,000?",
+      options: ["Yes", "No", "Prefer not to say"],
+      category: "multiple-choice",
+    },
+    {
+      question: "Is your household income below $50,000?",
+      options: ["Yes", "No", "Prefer not to say"],
+      category: "multiple-choice",
+    },
+    {
       question: "Select your state of residence:",
       options: ["State 1", "State 2", "State 3"],
       category: "dropdown",
+    },
+    {
+      question: "Select your state of residence:",
+      options: ["State 1", "State 2", "State 3"],
+      category: "dropdown",
+    },
+    {
+      question: "Select your state of residence:",
+      options: ["State 1", "State 2", "State 3"],
+      category: "dropdown",
+    },
+    {
+      question: "Select your preferred contact method:",
+      options: ["Email", "Phone", "Mail"],
+      category: "radio",
+    },
+    {
+      question: "Select your preferred contact method:",
+      options: ["Email", "Phone", "Mail"],
+      category: "radio",
     },
     {
       question: "Select your preferred contact method:",
--- src/questions/disabilityBenefitQs.js
+++ src/questions/disabilityBenefitQs.js
@@ -1,30 +1,59 @@
 const questions = [
-    {
-      question: "Do you have children under 18 years old?",
-      options: ["Yes", "No", "Prefer not to say"],
-      category: "multiple-choice",
-    },
-    {
-      question: "Is your household income below $50,000?",
-      options: ["Yes", "No", "Prefer not to say"],
-      category: "multiple-choice",
-    },
-    {
-      question: "Select your state of residence:",
-      options: ["State 1", "State 2", "State 3"],
-      category: "dropdown",
-    },
-    {
-      question: "Select your preferred contact method:",
-      options: ["Email", "Phone", "Mail"],
-      category: "radio",
-    },
-    {
-      question: "Do you have any disabilities?",
-      options: ["Yes", "No", "Prefer not to say"],
-      category: "multiple-choice",
-    },
-  ];
-  
-  export default questions;
-  
+  {
+    question: "Do you have children under 18 years old?",
+    options: ["Yes", "No", "Prefer not to say"],
+    category: "multiple-choice",
+  },
+  {
+    question: "Is your household income below $50,000?",
+    options: ["Yes", "No", "Prefer not to say"],
+    category: "multiple-choice",
+  },
+  {
+    question: "Is your household income below $50,000?",
+    options: ["Yes", "No", "Prefer not to say"],
+    category: "multiple-choice",
+  },
+  {
+    question: "Is your household income below $50,000?",
+    options: ["Yes", "No", "Prefer not to say"],
+    category: "multiple-choice",
+  },
+  {
+    question: "Select your state of residence:",
+    options: ["State 1", "State 2", "State 3"],
+    category: "dropdown",
+  },
+  {
+    question: "Select your state of residence:",
+    options: ["State 1", "State 2", "State 3"],
+    category: "dropdown",
+  },
+  {
+    question: "Select your state of residence:",
+    options: ["State 1", "State 2", "State 3"],
+    category: "dropdown",
+  },
+  {
+    question: "Select your preferred contact method:",
+    options: ["Email", "Phone", "Mail"],
+    category: "radio",
+  },
+  {
+    question: "Select your preferred contact method:",
+    options: ["Email", "Phone", "Mail"],
+    category: "radio",
+  },
+  {
+    question: "Select your preferred contact method:",
+    options: ["Email", "Phone", "Mail"],
+    category: "radio",
+  },
+  {
+    question: "Do you have any disabilities?",
+    options: ["Yes", "No", "Prefer not to say"],
+    category: "multiple-choice",
+  },
+];
+
+export default questions;
