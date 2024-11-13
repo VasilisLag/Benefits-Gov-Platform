41492cf69ed8c1aef830ca9e101743b65461c93f
EMEA\LAGOPV <vlagop98@gmail.com>
2024-11-13 10:38:30 +0200

    Modify: Questions about the heating benefit were added

--- src/questions/heatingBenefitQs.js
+++ src/questions/heatingBenefitQs.js
@@ -1,30 +1,66 @@
 const questions = [
-    {
-      question: "Do you have children under 18 years old?",
-      options: ["Yes", "No", "Prefer not to say"],
-      category: "multiple-choice",
-    },
-    {
-      question: "Is your household income below $50,000?",
-      options: ["Yes", "No", "Prefer not to say"],
-      category: "multiple-choice",
-    },
-    {
-      question: "Select your state of residence:",
-      options: ["State 1", "State 2", "State 3"],
-      category: "dropdown",
-    },
-    {
-      question: "Select your preferred contact method:",
-      options: ["Email", "Phone", "Mail"],
-      category: "radio",
-    },
-    {
-      question: "Do you have any disabilities?",
-      options: ["Yes", "No", "Prefer not to say"],
-      category: "multiple-choice",
-    },
-  ];
-  
-  export default questions;
-  
+  {
+    question: "Έχετε υποβάλει δήλωση φορολογίας εισοδήματος το τρέχον έτος;",
+    options: ["Ναι", "Όχι"],
+    category: "multiple-choice",
+    required: true
+  },
+  {
+    question: "Ποιο είναι το συνολικό ετήσιο οικογενειακό εισόδημά σας σε ευρώ;",
+    category: "textbox",
+    required: true
+  },
+  {
+    question: `Πόσα παιδιά έχετε ως εξαρτώμενα μέλη;`,
+    category: "textbox",
+    note: `
+    <ul style="list-style-type: disc; padding-left:30px; text-align:left;">
+      <li>Τέκνα από γάμο, φυσικά, θετά ή αναγνωρισμένα, εφόσον είναι άγαμα και:
+        <ul style="list-style-type: circle; padding-left:20px;">
+          <li>δεν υπερβαίνουν το 18ο έτος της ηλικίας τους</li>
+          <li>το 19ο έτος, αν φοιτούν στη μέση εκπαίδευση</li>
+        </ul>
+      </li>
+      <li>Τέκνα που φοιτούν σε ανώτερη ή ανώτατη εκπαίδευση, έως το 24ο έτος</li>
+      <li>Τέκνα με ποσοστό αναπηρίας 67% και άνω</li>
+    </ul><br>`,
+    required: true
+  },
+  {
+    question: "Είστε έγγαμος/η ή έχετε σύμφωνο συμβίωσης;",
+    options: ["Ναι", "Όχι"],
+    category: "multiple-choice",
+    required: true
+  },
+  {
+    question: "Η οικογένειά σας είναι μονογονεϊκή;",
+    options: ["Ναι", "Όχι"],
+    category: "multiple-choice",
+    required: true
+  },
+  {
+    question: "Ποια είναι η συνολική αντικειμενική αξία της ακίνητης περιουσίας σας (βάσει ΕΝ.Φ.Ι.Α. 2023) σε ευρώ;",
+    category: "textbox",
+    note: `Αυτό αφορά τη συνολική αξία της περιουσίας του αιτούντος, του/της συζύγου ή μέρους συμφώνου συμβίωσης και εξαρτώμενων τέκνων.`,
+    required: true
+  },
+  {
+    question: "Ασκείτε επιχειρηματική δραστηριότητα;",
+    options: ["Ναι", "Όχι"],
+    category: "multiple-choice",
+    required: true
+  },
+  {
+    question: "Εάν ασκείτε επιχειρηματική δραστηριότητα, ποια είναι τα συνολικά ακαθάριστα έσοδά σας από αυτή τη δραστηριότητα σε ευρώ;",
+    category: "textbox",
+    note: `Αφορά τα συνολικά ακαθάριστα έσοδα του αιτούντος, του/της συζύγου ή μέρους συμφώνου συμβίωσης και των εξαρτώμενων τέκνων.`,
+    required: false
+  },
+  {
+    question: "Πόσα χρόνια διαμένετε μόνιμα στην Ελλάδα;",
+    category: "textbox",
+    required: true
+  }
+];
+
+export default questions;
