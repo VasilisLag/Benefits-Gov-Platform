6652897c24d108c998e7cfd02b268fed831e845a
VasilisLag <vlagop98@gmail.com>
2025-06-22 21:58:43 +0300

    Modify: BenefitForm Layout includes 2 sections, one with question form and one with categories

--- src/components/Elements/Layouts/BenefitFormLayout.vue
+++ src/components/Elements/Layouts/BenefitFormLayout.vue
@@ -2,41 +2,66 @@
   <div class="page">
     <HeaderElement />
     <NavElement />
-    <main class="p-4">
-      <div class="form-container govgr-mt-12">
-        <QuestionForm 
-          :title="title"
-          :isLastQuestion="isLastQuestion"
-          :isFirstQuestion="isFirstQuestion"
-          :selectedOption="currentOption"
-          :isQuestionRequired="isQuestionReq"
-          :questionIndex="currentQuestionIndex"
-          :numberOfQuestions="questions.length"
-          @back="goBack"
-          @skip="skipQuestion"
-          @next="nextQuestion"
-          @submit="submitAnswers"
-        >
-          <InputElement
-            v-if="currentQuestion"
-            :question="currentQuestion.question"
-            :key="currentQuestionIndex"
-            :options="currentQuestion.options"
-            :category="currentQuestion.category"
-            :note="currentQuestion.note"
-            :input="answer"
-            :answer="answer"
-            @onAnswerChange="handleAnswerChange"
-          />
-          <SummaryTable v-if="isFormSummary"
-            :questions="questions.map(q => q.question)"
-            :answers="answers"
-          />
-        </QuestionForm>
+    
+    <div class="govgr-layout-wrapper govgr-mt-6">
+      <div class="govgr-width-container" id="layout-wrapper">
+        <div class="govgr-main-wrapper">
+
+          <!-- 2/3 Ενότητα ερωτήσεων -->
+          <main class="">
+            <QuestionForm 
+              :title="title"
+              :isLastQuestion="isLastQuestion"
+              :isFirstQuestion="isFirstQuestion"
+              :selectedOption="currentOption"
+              :isQuestionRequired="isQuestionReq"
+              :questionIndex="currentQuestionIndex"
+              :numberOfQuestions="questions.length"
+              @back="goBack"
+              @skip="skipQuestion"
+              @next="nextQuestion"
+              @submit="submitAnswers"
+            >
+              <InputElement
+                v-if="currentQuestion"
+                :question="currentQuestion.question"
+                :key="currentQuestionIndex"
+                :options="currentQuestion.options"
+                :category="currentQuestion.category"
+                :note="currentQuestion.note"
+                :input="answer"
+                :answer="answer"
+                @onAnswerChange="handleAnswerChange"
+              />
+              <SummaryTable v-if="isFormSummary"
+                :questions="questions.map(q => q.question)"
+                :answers="answers"
+              />
+            </QuestionForm>
+          </main>
+
+          <!-- 1/3 Sidebar με πληροφορία κατηγορίας -->
+          <aside class="govgr-aside">
+            <h2 class="govgr-heading-m">Κατηγορίες</h2>
+            <dl class="govgr-summary-list">
+              <div
+                v-for="(tag, index) in uniqueTags"
+                :key="index"
+                class="govgr-summary-list__row"
+                :class="{ 'gov-active-color': tag === currentTag }"
+              >
+                <dd class="govgr-summary-list__value">{{ formatTag(tag) }}</dd>
+              </div>
+            </dl>
+          </aside>
+
+        </div>
       </div>
-    </main>
+    </div>
+
   </div>
 </template>
+
 
 <script>
 import HeaderElement from '@/components/Elements/Page Elements/HeaderElement.vue';
@@ -83,7 +108,14 @@ export default {
     },
     isQuestionReq() {
       return this.currentQuestion?.required ?? false;
-    }
+    },
+    uniqueTags() {
+    const tags = this.questions.map(q => q.tag);
+    return [...new Set(tags)];
+  },
+    currentTag() {
+      return this.questions[this.currentQuestionIndex]?.tag;
+  }
   },
   methods: {
     handleAnswerChange(option) {
@@ -105,6 +137,17 @@ export default {
     },
     submitAnswers() {
       this.$emit('submit-answers', this.answers);
+    },
+    formatTag(tag) {
+      const map = {
+        demography: 'Δημογραφικά',
+        income: 'Εισόδημα',
+        household: 'Οικογενειακή Κατάσταση',
+        assets: 'Ακίνητα / Περιουσιακά Στοιχεία',
+        vulnerable: 'Ευάλωτες Ομάδες',
+        residence: 'Κατοικία'
+      };
+      return map[tag] || tag;
     }
   }
 };
@@ -112,19 +155,46 @@ export default {
 
 <style scoped>
 
-.form-container {
-  width: 70%;
-  margin: 0 auto;
-}
-
 .page {
   display:flex;
   flex-direction: column;
   min-height: 100vh;
-  overflow-y: scroll;
 }
 
+.govgr-main-wrapper{
+  display: grid;
+  grid-template-columns: 2fr 1fr; 
+}
+
+.govgr-aside {
+  margin-top: 4rem;
+  align-self: start;
+}
+
+.govgr-summary-list {
+ 
+  margin-bottom:0;
+}
+
+.govgr-aside ul {
+  padding-left: 1rem;
+}
+
+.govgr-aside li {
+  text-align: left;
+  padding: 0.4rem 0;
+}
+.gov-active-color{
+  color: #003375;
+  font-weight: bold;
+}
 main {
   flex: 1;
 }
+
+#layout-wrapper {
+  margin-left: 15%;
+  max-width: 80%;
+}
+
 </style>
