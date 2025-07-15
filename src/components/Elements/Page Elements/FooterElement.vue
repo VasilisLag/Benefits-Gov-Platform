c17e641e8ce27a48f7e863a9237a58b13f4cb1c8
VasilisLag <vlagop98@gmail.com>
2025-07-15 22:51:46 +0300

    Modify: Footer changed to indicate the purpose of the web platform development

--- src/components/Elements/Page Elements/FooterElement.vue
+++ src/components/Elements/Page Elements/FooterElement.vue
@@ -4,10 +4,9 @@
             <div class="govgr-footer__meta">
                 <div class="govgr-footer__meta-item govgr-footer__meta-item--grow">
                     <div class="govgr-footer__content">
-                        <p class="govgr-footer__licence-description">
-                            Υλοποίηση από το 
-                            <a href="https://grnet.gr" target="_blank" rel="noreferrer noopener" class="govgr-link">ΕΔΥΤΕ<span class="govgr-visually-hidden">(ανοίγει σε καινούρια καρτέλα)</span></a> για το
-                            <a href="https://mindigital.gr/" target="_blank" rel="noreferrer noopener" class="govgr-link"> Υπουργείο Ψηφιακής Διακυβέρνησης <span class="govgr-visually-hidden">(ανοίγει σε καινούρια καρτέλα)</span></a>
+                        <p class="govgr-footer__licence-description" style="text-align: left;">
+                            Αυτή η διαδικτυακή πλατφόρμα αναπτύχθηκε στο πλαίσιο διπλωματικής εργασίας του Μεταπτυχιακού Προγράμματος Σπουδών Εφαρμοσμένης Πληροφορικής 
+                            του Πανεπιστημίου Μακεδονίας απο τον φοιτητή Βασίλειο Λαγόπουλο
                         </p>
                     </div>
                 </div>
